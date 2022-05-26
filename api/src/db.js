988f460aead700dcb4ad69dde21b20709b872f6d
Guillermo Jofre <jofregf@gmail.com>
2022-05-26 11:45:56 -0300

    prueba

--- api/src/db.js
+++ api/src/db.js
@@ -71,3 +71,4 @@ module.exports = {
   ...sequelize.models, // para poder importar los modelos así: const { Product, User } = require('./db.js');
   conn: sequelize,     // para importart la conexión { conn } = require('./db.js');
 };
+
