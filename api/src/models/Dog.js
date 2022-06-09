33d58c489a1060fae4cf0cdb5b750cde7d6f243c
Guillermo Jofre <jofregf@gmail.com>
2022-06-09 10:36:21 -0300

    Probando

--- api/src/models/Dog.js
+++ api/src/models/Dog.js
@@ -4,17 +4,17 @@ const { DataTypes } = require('sequelize');
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('dog', {
-    name: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-
     id: {
       type: DataTypes.UUID,
       defaultValue: DataTypes.UUIDV4,
       allowNull: false,
       primaryKey: true,
     },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+
 
     height_min: {
       type: DataTypes.INTEGER,
